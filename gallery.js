5fcc5c6bacd028f3838fcd9e13a586c13fe027d3
Lisa Maagdenberg <lisa.maagdenberg92@gmail.com>
2017-01-21 17:27:00 +0100

    added rounded corners, deleted attempt to make a modal

---
+++ gallery.js
@@ -0,0 +1,11 @@
+// click event, bijv. $({name}).click(function () {
+	// doe iets
+	// pak mijn grote img obv wat geclickt is
+	// zet die in het midden, goed gescaled etc.
+	// maak de rest (doorzichtig) zwart
+	// close --> keybindings 
+	// if (keypress = 38) {close}
+	// if (click on black area) {close}
+	// if (click on button) {close}
+// })
+
