050c4985257a9b24bb3b04d839a152906d6a1706
Hajar Mokhtafa <hajarmokhtafa@gmail.com>
2017-01-22 10:17:04 +0100

    some changes, melvin will take a look

--- subscription.js
+++ subscription.js
@@ -1,4 +1,89 @@
+$(document).ready(function(){
+
+
+/* This is for <h1> Should show one sentence at a time that changes over time. Doesn't do that right now. When I try it on CodePen, 
+it works perfectly, but not here. */
+
+
+	(function($){
+    $.fn.extend({ 
+        rotaterator: function(options) {
+ 
+            var defaults = {
+                fadeSpeed: 500,
+                pauseSpeed: 100,
+				child:null
+            };
+             
+            var options = $.extend(defaults, options);
+         
+            return this.each(function() {
+                  var o =options;
+                  var obj = $(this);                
+                  var items = $(obj.children(), obj);
+				  items.each(function() {$(this).hide();})
+				  if(!o.child){var next = $(obj).children(':first');
+				  }else{var next = o.child;
+				  }
+				  $(next).fadeIn(o.fadeSpeed, function() {
+						$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+							var next = $(this).next();
+							if (next.length == 0){
+									next = $(obj).children(':first');
+							}
+							$(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
+						})
+					});
+            });
+        }
+    });
+})(jQuery);
+
+ $(document).ready(function() {
+        $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:100});
+ });
+
+});
+
+
+
+
+
+/* this is for the button "click for price". When you click on it, a modal should appear with the price. Doesn't work. Not sure why. 
+When I try it on CodePen it does work. Not sure what I am doing wrong? */
+
+
 jQuery(document).ready(function($){
+
+// Click function for show the Modal
+
+$(".show").on("click", function(){
+  $(".mask").addClass("active");
+});
+
+// Function for close the Modal
+
+function closeModal(){
+  $(".mask").removeClass("active");
+}
+
+// Call the closeModal function on the clicks/keyboard
+
+$(".close, .mask").on("click", function(){
+  closeModal();
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+});
+
+
+
+	/* Doesn't work at all for me. Could it be because of the footer that I have integrated? It did work for Toska, so not sure why it doesn't
+	work for me? Not so important, will try it when I have fixed the other problems.
+
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
@@ -25,4 +110,4 @@ jQuery(document).ready(function($){
 		);
 	});
 
-}); 
+	*/ 
