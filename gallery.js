a7185f5c328fa3d7e586baa6c4308206f14dba4d
Lisa Maagdenberg <lisa.maagdenberg92@gmail.com>
2017-01-22 11:57:33 +0100

    Text over image on hover, but ugly coding

--- gallery.js
+++ gallery.js
@@ -1,11 +1,42 @@
-// click event, bijv. $({name}).click(function () {
-	// doe iets
-	// pak mijn grote img obv wat geclickt is
-	// zet die in het midden, goed gescaled etc.
-	// maak de rest (doorzichtig) zwart
-	// close --> keybindings 
-	// if (keypress = 38) {close}
-	// if (click on black area) {close}
-	// if (click on button) {close}
-// })
 
+
+// Ugly code, but for now the only way it will work... 
+// This functions make text hover over the images
+$(document).ready(function(){
+	$('#photo1').mouseenter(function(){
+		$('#text1').fadeIn(800);
+	});
+	$('#photo1').mouseleave(function(){
+		$('#text1').fadeOut(800);
+	});
+	$('#photo2').mouseenter(function(){
+		$('#text2').fadeIn(800);
+	});
+	$('#photo2').mouseleave(function(){
+		$('#text2').fadeOut(800);
+	});
+	$('#photo3').mouseenter(function(){
+		$('#text3').fadeIn(800);
+	});
+	$('#photo3').mouseleave(function(){
+		$('#text3').fadeOut(800);
+	});
+	$('#photo4').mouseenter(function(){
+		$('#text4').fadeIn(800);
+	});
+	$('#photo4').mouseleave(function(){
+		$('#text4').fadeOut(800);
+	});
+	$('#photo5').mouseenter(function(){
+		$('#text5').fadeIn(800);
+	});
+	$('#photo5').mouseleave(function(){
+		$('#text5').fadeOut(800);
+	});
+	$('#photo6').mouseenter(function(){
+		$('#text6').fadeIn(800);
+	});
+	$('#photo6').mouseleave(function(){
+		$('#text6').fadeOut(800);
+	});
+});
